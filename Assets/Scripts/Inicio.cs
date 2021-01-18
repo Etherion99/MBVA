aab1a9a2de70b527f0d09fc08a9885dcd15826b5
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-01-18 18:16:15 -0500

    escena inicial añadida

---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour
+{
+    public Camera camara1, camara2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void info()
+    {
+        camara2.enabled = true;
+        camara1.enabled = false;
+
+        camara2.GetComponent<AudioListener>().enabled = true;
+        camara1.GetComponent<AudioListener>().enabled = false;
+    }
+}
