1f92c3f6823b74e8595eeb6091227d2236612b2b
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2021-01-18 20:48:04 -0500

    Escena info

--- Assets/Scripts/Inicio.cs
+++ Assets/Scripts/Inicio.cs
@@ -25,4 +25,13 @@ public class Inicio : MonoBehaviour
         camara2.GetComponent<AudioListener>().enabled = true;
         camara1.GetComponent<AudioListener>().enabled = false;
     }
+
+    public void info1()
+    {
+        camara1.enabled = true;
+        camara2.enabled = false;
+
+        camara1.GetComponent<AudioListener>().enabled = true;
+        camara2.GetComponent<AudioListener>().enabled = false;
+    }
 }
